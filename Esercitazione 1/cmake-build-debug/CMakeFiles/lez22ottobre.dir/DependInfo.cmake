c111664fcda7c4ac3178f789b7441edbe02ad1d2
Alessandro <sanna.alessandro.1996@gmail.com>
2021-10-25 18:45:37 +0200

    prima esercitazione

---
+++ Esercitazione 1/cmake-build-debug/CMakeFiles/lez22ottobre.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/alessandro/Code/PAIS/anno21_22/esercitazioni/lez22ottobre/esercizio_1.c" "/home/alessandro/Code/PAIS/anno21_22/esercitazioni/lez22ottobre/cmake-build-debug/CMakeFiles/lez22ottobre.dir/esercizio_1.c.o"
+  "/home/alessandro/Code/PAIS/anno21_22/esercitazioni/lez22ottobre/esercizio_2.c" "/home/alessandro/Code/PAIS/anno21_22/esercitazioni/lez22ottobre/cmake-build-debug/CMakeFiles/lez22ottobre.dir/esercizio_2.c.o"
+  "/home/alessandro/Code/PAIS/anno21_22/esercitazioni/lez22ottobre/esercizio_2_alt.c" "/home/alessandro/Code/PAIS/anno21_22/esercitazioni/lez22ottobre/cmake-build-debug/CMakeFiles/lez22ottobre.dir/esercizio_2_alt.c.o"
+  "/home/alessandro/Code/PAIS/anno21_22/esercitazioni/lez22ottobre/esercizio_3.c" "/home/alessandro/Code/PAIS/anno21_22/esercitazioni/lez22ottobre/cmake-build-debug/CMakeFiles/lez22ottobre.dir/esercizio_3.c.o"
+  "/home/alessandro/Code/PAIS/anno21_22/esercitazioni/lez22ottobre/esercizio_4.c" "/home/alessandro/Code/PAIS/anno21_22/esercitazioni/lez22ottobre/cmake-build-debug/CMakeFiles/lez22ottobre.dir/esercizio_4.c.o"
+  "/home/alessandro/Code/PAIS/anno21_22/esercitazioni/lez22ottobre/main.c" "/home/alessandro/Code/PAIS/anno21_22/esercitazioni/lez22ottobre/cmake-build-debug/CMakeFiles/lez22ottobre.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Esercitazione 1/cmake-build-debug/CMakeFiles/lez22ottobre.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lez22ottobre.dir/esercizio_1.c.o"
+  "CMakeFiles/lez22ottobre.dir/esercizio_2.c.o"
+  "CMakeFiles/lez22ottobre.dir/esercizio_2_alt.c.o"
+  "CMakeFiles/lez22ottobre.dir/esercizio_3.c.o"
+  "CMakeFiles/lez22ottobre.dir/esercizio_4.c.o"
+  "CMakeFiles/lez22ottobre.dir/main.c.o"
+  "lez22ottobre"
+  "lez22ottobre.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lez22ottobre.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
